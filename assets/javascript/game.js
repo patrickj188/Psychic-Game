260d61fbd169e3b43f0ad332effcd01f166d66ad
patrickj188@gmail.com <Patrickj188@gmail.com>
2018-12-14 11:32:47 -0600

    v5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,42 @@
-var wins=0;
-var losses=0;
-var guesses =[];
+var wins = 0;
+var losses = 0;
+var guesses = [];
 var numGuesses = 9;
 
-var winsText= document.getElementById("wins-text");
-var lossesText= document.getElementById("losses-text");
-var guessText= document.getElementById("guess-text");
-var numberOfGuesses= document.getElementById("guessesleft-text")
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessText = document.getElementById("guess-text");
+var numberOfGuesses = document.getElementById("guessesleft-text")
 
-var computerChoices = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-document.onkeyup = function(event) {
-  var userGuess = event.key;
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  
-
-  if(userGuess === computerGuess){
-    wins++
-    numGuesses= 9;
-    guesses = []
-    
-  }else if(userGuess !== computerChoices){
-      numGuesses --;
-      guesses.push(userGuess);
-  }
-
-  if (numGuesses === 0){
-      numGuesses= 9;
-      losses ++;
-      guesses = []
-  }
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 
-  numberOfGuesses.textContent=('Number of guesses: ' + numGuesses);
-  winsText.textContent=("Wins: " + wins);
-  lossesText.textContent=("Losses: " + losses);
-  guessText.textContent=("You guessed: " + guesses.join([","]));
+    if (userGuess === computerGuess) {
+        wins++;
+        numGuesses = 9;
+        guesses = [];
 
+    } else if (userGuess !== computerChoices) {
+        numGuesses--;
+        guesses.push(userGuess);
+    }
+
+    if (numGuesses === 0) {
+        numGuesses = 9;
+        losses++;
+        guesses = [];
+    }
+
+
+numberOfGuesses.textContent = ('Number of guesses: ' + numGuesses);
+winsText.textContent = ("Wins: " + wins);
+lossesText.textContent = ("Losses: " + losses);
+guessText.textContent = ("You guessed: " + guesses.join([","]).toUpperCase());
 
 };
+
+
